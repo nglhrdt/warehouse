9276933e8139522b766e299b55fb5fa0265a041e
Simon Engelhardt <smn@nglhrdt.de>
2024-11-28 20:21:04 +0000

    render slot grid

--- packages/ui/src/components/organizer-list-item.tsx
+++ packages/ui/src/components/organizer-list-item.tsx
@@ -1,7 +1,7 @@
 import api from '@/api';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { OrganizerDTO } from 'api';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { LuTrash } from 'react-icons/lu';
 
 interface OrganizerListItemProps {
@@ -12,9 +12,12 @@ const OrganizerListItem: React.FC<OrganizerListItemProps> = ({ organizer }) => {
   const deleteMutation = useMutation({ mutationFn: () => api.deleteOrganizer(organizer.id) });
   const queryClient = useQueryClient();
 
-  function deleteOrganizer(): void {
-    deleteMutation.mutateAsync().then(() => queryClient.invalidateQueries({ queryKey: ['organizer'] }));
-  }
+  const deleteOrganizer = useCallback(
+    () => deleteMutation.mutateAsync().then(() => queryClient.invalidateQueries({ queryKey: ['organizer'] })),
+    [deleteMutation, queryClient],
+  );
+
+  const { columns, rows } = organizer;
 
   return (
     <div
@@ -22,8 +25,28 @@ const OrganizerListItem: React.FC<OrganizerListItemProps> = ({ organizer }) => {
       key={organizer.id}
     >
       <div className="flex justify-between items-center">
-        {organizer.name}
+        <div className="flex gap-4">
+          {organizer.name} cols: {columns} rows: {rows}
+        </div>
         <LuTrash onClick={deleteOrganizer} />
+      </div>
+      <div
+        className={`grid border border-gray-300`}
+        style={{
+          gridTemplateColumns: `repeat(${columns}, minmax(0, 1fr))`,
+          gridTemplateRows: `repeat(${rows}, minmax(0, 1fr))`,
+        }}
+      >
+        {Array.from({ length: rows }).map((_, rowIndex) =>
+          Array.from({ length: columns }).map((_, colIndex) => (
+            <div
+              key={`${rowIndex}-${colIndex}`}
+              className="border border-gray-300 p-2"
+            >
+              {`Row ${rowIndex + 1}, Col ${colIndex + 1}`}
+            </div>
+          )),
+        )}
       </div>
     </div>
   );
