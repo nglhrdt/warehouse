304448642d2aa480d634b8bac7d1ed6d763ce9c5
Simon Engelhardt <smn@nglhrdt.de>
2025-04-07 18:23:31 +0000

    refactor itemsService to improve item handling and ensure consistent return types

--- src/v1/services/itemsService.ts
+++ src/v1/services/itemsService.ts
@@ -2,6 +2,12 @@ import { MongoClient, ObjectId } from "mongodb";
 import MongoClientSingleton from "../../utils/mongoClient.ts";
 import { DB_NAME, ITEMS_COLLECTION } from "../config/constants.ts";
 
+type Item = {
+  id: string;
+  name: string;
+  description: string;
+};
+
 class ItemsService {
   private client: MongoClient;
 
@@ -15,30 +21,39 @@ class ItemsService {
 
   async getAllItems() {
     const collection = await this.getCollection();
-    return await collection.find({}).toArray();
+    return (await collection.find({}).toArray()).map(this.toItem);
   }
 
   async getItemById(id: string) {
     const collection = await this.getCollection();
-    return await collection.findOne({ _id: new ObjectId(id) });
+    const model = await collection.findOne({ _id: new ObjectId(id) });
+    return model ? this.toItem(model) : null;
   }
 
   async createItem(item: any) {
     const collection = await this.getCollection();
     const result = await collection.insertOne(item);
-    return { id: result.insertedId, ...item };
+    return this.toItem({ _id: result.insertedId, ...item });
   }
 
   async updateItem(id: string, item: any) {
     const collection = await this.getCollection();
     await collection.updateOne({ _id: new ObjectId(id) }, { $set: item });
-    return { id, ...item };
+    return this.toItem({ _id: id, ...item });
   }
 
   async deleteItem(id: string) {
     const collection = await this.getCollection();
     await collection.deleteOne({ _id: new ObjectId(id) });
     return { message: `Item with id ${id} deleted` };
+  }
+
+  toItem(model): Item {
+    return {
+      id: model._id.toString(),
+      name: model.name,
+      description: model.description,
+    };
   }
 }
 
